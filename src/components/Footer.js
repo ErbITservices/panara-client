e85d7b41620d8132989d31d8f9a50dc1b8a25661
MIHIRPATEL2715 <mihirpqtel@gmail.com>
2024-12-05 19:04:56 +0530

    mihir

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -152,7 +152,7 @@ function Footer() {
               <ListItem> Register Complain </ListItem>
             </Link>
             <Link to={"/login"}>
-              <ListItem> Login  </ListItem>
+              <ListItem> Login - </ListItem>
               <br/>
             </Link>
             <Link to={"/signup"}>
